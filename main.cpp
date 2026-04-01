1911a51b09c5468e52552201a648f231fe44e555
iannathan-k <ianmtv@gmail.com>
2026-04-01 12:11:48 -0700

    working on insertion

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <list>
 #include <set>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 using namespace std::chrono;
 
@@ -11,6 +12,7 @@ using namespace std::chrono;
 
 const int SIZE = 20000;
 
+void displayTimes(string row_name, nanoseconds v, nanoseconds l, nanoseconds s);
 void timeReading(vector<string>& v, list<string>& l, set<string>& s);
 
 int main() {
@@ -18,6 +20,14 @@ int main() {
     vector<string> v;
     list<string> l;
     set<string> s;
+
+    // Display header
+    cout << setw(10) << "Operation";
+    cout << setw(10) << right << "Vector";
+    cout << setw(10) << right << "List";
+    cout << setw(10) << right << "Set" << endl;
+
+    timeReading(v, l, s);
 
     return 0;
 }
@@ -28,6 +38,13 @@ auto end = high_resolution_clock::now()
 auto duration = duration_cast<milliseconds>(end - start)
 duration.count() references elapsed milliseconds
 */
+
+void displayTimes(string row_name, nanoseconds v, nanoseconds l, nanoseconds s) {
+    cout << setw(10) << right << row_name;
+    cout << setw(10) << right << v.count();
+    cout << setw(10) << right << l.count();
+    cout << setw(10) << right << s.count() << endl;
+}
 
 void timeReading(vector<string>& v, list<string>& l, set<string>& s) {
     ifstream fin;
@@ -72,4 +89,55 @@ void timeReading(vector<string>& v, list<string>& l, set<string>& s) {
 
     end = high_resolution_clock::now();
     auto s_duration = duration_cast<nanoseconds>(end - start);
+
+    displayTimes("Read", v_duration, l_duration, s_duration);
+}
+
+void timeSorting(vector<string>& v, list<string>& l) {
+    // VECTOR TESTING
+    auto start = high_resolution_clock::now();
+
+    sort(v.begin(), v.end());
+
+    auto end = high_resolution_clock::now();
+    auto v_duration = duration_cast<nanoseconds>(end - start);
+
+    // LIST TESTING
+    start = high_resolution_clock::now();
+    l.sort();
+
+    end = high_resolution_clock::now();
+    auto l_duration = duration_cast<nanoseconds>(end - start);
+
+    // SET TESTING
+    nanoseconds s_duration(-1);
+
+    displayTimes("Sort", v_duration, l_duration, s_duration);
+}
+
+void timeInserting(vector<string>& v, list<string>& l, set<string>& s) {
+    // VECTOR TESTING
+    auto start = high_resolution_clock::now();
+
+    v.insert(v.begin() + v.size() / 2, "TESTCODE");
+
+    auto end = high_resolution_clock::now();
+    auto v_duration = duration_cast<nanoseconds>(end - start);
+
+    // LIST TESTING
+    start = high_resolution_clock::now();
+
+    for (int i = 0; i < SIZE; i++) {
+        auto it = l.begin();
+        advance(it, l.size() / 2);
+        l.insert(it, "code");
+    }
+
+    end = high_resolution_clock::now();
+    auto l_duration = duration_cast<nanoseconds>(end - start);
+
+    // SET TESTING
+    nanoseconds s_duration(-1);
+
+    displayTimes("Insert", v_duration, l_duration, s_duration);
 }
