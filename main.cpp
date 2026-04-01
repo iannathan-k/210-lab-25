addc92830ab4d477b0c9ba5394dac0a5b0066a7a
iannathan-k <ianmtv@gmail.com>
2026-04-01 12:22:16 -0700

    working on commenting

--- main.cpp
+++ main.cpp
@@ -10,10 +10,13 @@ using namespace std::chrono;
 
 // COMSC-210 | Lab 25 | Ian Kusmiantoro
 
-const int SIZE = 20000;
+const int COLUMN_WIDTH = 15;
 
-void displayTimes(string row_name, nanoseconds v, nanoseconds l, nanoseconds s);
-void timeReading(vector<string>& v, list<string>& l, set<string>& s);
+void displayTimes(string, nanoseconds, nanoseconds, nanoseconds);
+void timeReading(vector<string>&, list<string>&, set<string>&);
+void timeSorting(vector<string>&, list<string>&);
+void timeInserting(vector<string>&, list<string>&, set<string>&);
+void timeDeleting(vector<string>&, list<string>&, set<string>&);
 
 int main() {
     
@@ -22,12 +25,15 @@ int main() {
     set<string> s;
 
     // Display header
-    cout << setw(10) << "Operation";
-    cout << setw(10) << right << "Vector";
-    cout << setw(10) << right << "List";
-    cout << setw(10) << right << "Set" << endl;
+    cout << setw(COLUMN_WIDTH) << "Operation";
+    cout << setw(COLUMN_WIDTH) << right << "Vector";
+    cout << setw(COLUMN_WIDTH) << right << "List";
+    cout << setw(COLUMN_WIDTH) << right << "Set" << endl;
 
     timeReading(v, l, s);
+    timeSorting(v, l);
+    timeInserting(v, l, s);
+    timeDeleting(v, l, s);
 
     return 0;
 }
@@ -40,10 +46,10 @@ duration.count() references elapsed milliseconds
 */
 
 void displayTimes(string row_name, nanoseconds v, nanoseconds l, nanoseconds s) {
-    cout << setw(10) << right << row_name;
-    cout << setw(10) << right << v.count();
-    cout << setw(10) << right << l.count();
-    cout << setw(10) << right << s.count() << endl;
+    cout << setw(COLUMN_WIDTH) << right << row_name;
+    cout << setw(COLUMN_WIDTH) << right << v.count();
+    cout << setw(COLUMN_WIDTH) << right << l.count();
+    cout << setw(COLUMN_WIDTH) << right << s.count() << endl;
 }
 
 void timeReading(vector<string>& v, list<string>& l, set<string>& s) {
@@ -127,17 +133,52 @@ void timeInserting(vector<string>& v, list<string>& l, set<string>& s) {
     // LIST TESTING
     start = high_resolution_clock::now();
 
-    for (int i = 0; i < SIZE; i++) {
-        auto it = l.begin();
-        advance(it, l.size() / 2);
-        l.insert(it, "code");
-    }
+    auto it = l.begin();
+    advance(it, l.size() / 2);
+    l.insert(it, "TESTCODE");
 
     end = high_resolution_clock::now();
     auto l_duration = duration_cast<nanoseconds>(end - start);
 
     // SET TESTING
-    nanoseconds s_duration(-1);
+    start = high_resolution_clock::now();
+
+    s.insert("TESTCODE");
+
+    end = high_resolution_clock::now();
+    auto s_duration = duration_cast<nanoseconds>(end - start);
 
     displayTimes("Insert", v_duration, l_duration, s_duration);
+}
+
+void timeDeleting(vector<string>& v, list<string>& l, set<string>& s) {
+    // VECTOR TESTING
+    auto start = high_resolution_clock::now();
+
+    v.erase(v.begin() + v.size() / 2);
+
+    auto end = high_resolution_clock::now();
+    auto v_duration = duration_cast<nanoseconds>(end - start);
+
+    // LIST TESTING
+    start = high_resolution_clock::now();
+
+    auto l_it = l.begin();
+    advance(l_it, l.size() / 2);
+    l.erase(l_it);
+
+    end = high_resolution_clock::now();
+    auto l_duration = duration_cast<nanoseconds>(end - start);
+
+    // SET TESTING
+    start = high_resolution_clock::now();
+
+    auto s_it = s.begin();
+    advance(s_it, s.size() / 2);
+    s.erase(s_it);
+
+    end = high_resolution_clock::now();
+    auto s_duration = duration_cast<nanoseconds>(end - start);
+
+    displayTimes("Delete", v_duration, l_duration, s_duration);
 }
